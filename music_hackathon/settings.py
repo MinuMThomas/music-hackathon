92f1f9c498c0bba39718b566212aa6f531f27985
SamuelUkachukwu <shybold@yahoo.com>
2022-11-18 23:24:42 +0000

    Installed allauth for authentication. Added the home templates, view and urls.

--- music_hackathon/settings.py
+++ music_hackathon/settings.py
@@ -41,6 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'game',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +76,16 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 WSGI_APPLICATION = 'music_hackathon.wsgi.application'
