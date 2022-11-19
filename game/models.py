09ca2e5a27328a10a9e61dee1c1922d2968f6b85
SamuelUkachukwu <shybold@yahoo.com>
2022-11-19 11:02:01 +0000

    Added Game Question model.

--- game/models.py
+++ game/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField()
+    audio_file = models.FileField(upload_to='media_audio/')
+    description = models.TextField(max_length=550)
+    clue1 = models.CharField(max_length=200)
+    clue2 = models.CharField(max_length=200)
+    clue3 = models.CharField(max_length=200)
+    clue4 = models.CharField(max_length=200)
+    answer_option1 = models.CharField(max_length=200)
+    answer_option2 = models.CharField(max_length=200)
+    answer_option3 = models.CharField(max_length=200)
+    answer_option4 = models.CharField(max_length=200)
+    answer = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- music_hackathon/settings.py
+++ music_hackathon/settings.py
@@ -65,13 +65,17 @@ ROOT_URLCONF = 'music_hackathon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.media',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -139,6 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
