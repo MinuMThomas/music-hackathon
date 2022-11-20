62d8588ca36de42a884164666f7b90800c6236f9
anyahush <anyahush@gmail.com>
2022-11-20 20:45:57 +0000

    Add to game functionality to allow user to select an answer

--- game/views.py
+++ game/views.py
@@ -5,43 +5,39 @@ from .models import Question
 
 
 def game(request):
-    questions = list(Question.objects.all())[:10]
-    random.shuffle(questions)
-    # if request.method == 'POST':
-    #     print(request.POST)
-    #     questions = Question.objects.all()
-    #     clueOne = Question.clue1
-    #     clueTwo = Question.clue2
-    #     score = 500
-    #     wrong = 0
-    #     correct = 0
-    #     total = 0
-    #     for q in questions:
-    #         total += 1
-    #         print(request.POST.get(q.question))
-    #         print(q.ans)
-    #         print()
-    #         if q.ans == request.POST.get(q.question):
-    #             score += 50
-    #             correct += 1
-    #         else:
-    #             wrong += 1
-    #     percent = score/(total*10) * 100
-    template_name = 'game/game.html'
-    context = {
-        'questions': questions
-        # 'score': score,
-        # 'time': request.POST.get('timer'),
-        # 'correct': correct,
-        # 'wrong': wrong,
-        # 'percent': percent,
-        # 'total': total
-    }
-    return render(request, template_name, context)
-    # else:
-    #     questions = Question.objects.all()
-    #     template_name = 'game/game.html'
-    #     context = {
-    #         'questions': questions
-    #     }
-    #     return render(request, template_name, context)
+    if request.method == 'POST':
+        questions = list(Question.objects.all())[:10]
+        random.shuffle(questions)
+        clueOne = Question.clue1
+        clueTwo = Question.clue2
+        score = 500
+        wrong = 0
+        correct = 0
+        total = 0
+        for q in questions:
+            total += 1
+            if q.answer == request.POST.get(q.name):
+                score += 50
+                correct += 1
+            else:
+                wrong += 1
+        percent = score/(total*10) * 100
+        template_name = 'game/game.html'
+        context = {
+            'questions': questions,
+            'score': score
+            # 'time': request.POST.get('timer'),
+            # 'correct': correct,
+            # 'wrong': wrong,
+            # 'percent': percent,
+            # 'total': total
+        }
+        return render(request, template_name, context)
+    else:
+        questions = list(Question.objects.all())[:10]
+        random.shuffle(questions)
+        template_name = 'game/game.html'
+        context = {
+            'questions': questions
+        }
+        return render(request, template_name, context)
